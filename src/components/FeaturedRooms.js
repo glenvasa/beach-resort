30eb47f33960d9b23bacb063e460ef5f5bf18ed7
Glen Vasa <glenvasa@gmail.com>
2020-12-02 16:50:43 -0500

    Create FeaturedRooms.js

---
+++ src/components/FeaturedRooms.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import { RoomContext } from "../context";
+
+export default class FeaturedRooms extends Component {
+  static contextType = RoomContext;
+  render() {
+    return <div></div>;
+  }
+}
